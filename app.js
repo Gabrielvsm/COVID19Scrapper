c7997e2c3e0b5ea9acbab2ba8dfece139f747244
Gabriel Vasconcelos <gabriel.vasc@hotmail.com>
2020-03-21 17:46:49 -0300

    App done

---
+++ app.js
@@ -0,0 +1,67 @@
+require('dotenv').config();
+
+const axios = require('axios');
+const cheerio = require('cheerio');
+
+const url = process.env.URL;
+let cases = {};
+let $;
+
+async function getCasesByCountry(country){
+    let html = await getHtml(country);
+    $ = cheerio.load(html);
+    cases = {};
+
+    let panels = $('.row').children('.col-md-6');
+
+    let activeCasesPanel = panels.first().find( $('.panel-body') );
+    let closedCasesPanel = panels.eq(1).find( $('.panel-body') );
+
+    cases = {
+        'total': getPrimary(activeCasesPanel) + getPrimary(closedCasesPanel),
+        'activeCases': {
+            'totalActiveCases': getPrimary(activeCasesPanel),
+            'mildConditionCases': getFloatLeft(activeCasesPanel),
+            'criticalCases': getFloatRight(activeCasesPanel)
+        },
+        'closedCases': {
+            'totalClosedCases': getPrimary(closedCasesPanel),
+            'recoveredCases': getFloatLeft(closedCasesPanel),
+            'deathCases': getFloatRight(closedCasesPanel)
+        }
+    };
+
+    return cases;
+}
+
+function getPrimary(panel){
+    let total = panel.find( $('.panel_front') ).children().first().text().trim().replace(",", "");
+    return parseInt(total);
+}
+
+function getFloatLeft(panel){
+    let leftDiv = panel.find( $('.panel_front') ).children().eq(2).children().first();
+    let leftValue = leftDiv.find( $('.number-table') ).text().trim().replace(",", "");
+    return parseInt(leftValue);
+}
+
+function getFloatRight(panel){
+    let rightDiv = panel.find( $('.panel_front') ).children().eq(2).children().eq(1);
+    let rightValue = rightDiv.find( $('.number-table') ).text().trim().replace(",", "");
+    return parseInt(rightValue);
+}
+
+async function getHtml(country){
+    let finalUrl = url.concat(country);
+    let html;
+
+    await axios(finalUrl).then(res => {
+        html = res.data;
+    }).catch( console.error );
+
+    return html;
+}
+
+module.exports = {
+    getCasesByCountry
+}
